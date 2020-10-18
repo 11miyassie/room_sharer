2e973297ddc71d0ba354cb44d755dc193ce51d82
11nekomamire <nekomamiremy@gmail.com>
2020-10-18 20:22:55 +0900

    ヘッダー調整、prefectureのactive_hash

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
   with_options presence: true do
     validates :title
