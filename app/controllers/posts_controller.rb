c4a699226c0a06ba3c94325301d92ad53e7a5d17
11nekomamire <nekomamiremy@gmail.com>
2020-10-18 23:23:47 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :destroy, :update]
+
   def index
     @posts = Post.includes(:user).order("created_at DESC")
   end
@@ -35,6 +38,6 @@ class PostsController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
