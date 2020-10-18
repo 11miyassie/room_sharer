9e2dd7a2177cbd9627e34ca3267fc7e8de8c2dec
11nekomamire <nekomamiremy@gmail.com>
2020-10-18 17:42:24 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+
+  resources :users
+  resources :posts
+  resources :rooms do
+    resources :messages
+  end
 end
