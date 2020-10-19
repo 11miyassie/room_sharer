6c8ca1f5a928f27e7c9d669d1e4dbe542ee64c9c
11nekomamire <nekomamiremy@gmail.com>
2020-10-19 16:41:10 +0900

    roomモデル

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :posts
-  resources :rooms do
-    resources :messages
+  resources :rooms, only: [:index, :new, :create, :destroy]do
+    resources :messages, only: [:index, :create]
   end
 end
