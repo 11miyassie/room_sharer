451bbfbba73a48db390a09ee2fe599dcc7822e9e
11nekomamire <nekomamiremy@gmail.com>
2020-10-19 16:51:21 +0900

    Room=> Validation, add Column

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,16 @@ class RoomsController < ApplicationController
 
   def new
   end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
