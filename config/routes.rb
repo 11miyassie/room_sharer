220a26d9ef892598225865da1eaf40a70b0abcd0
11nekomamire <nekomamiremy@gmail.com>
2020-10-20 09:44:24 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :posts
-  resources :rooms, only: [:index, :new, :create, :destroy]do
+  resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
