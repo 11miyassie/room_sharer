79e72dace98e7da814e977b833518355b249dfaa
11nekomamire <nekomamiremy@gmail.com>
2020-10-20 09:45:08 +0900

    Update room.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,9 @@
 class Room < ApplicationRecord
   has_many :room_users
-  has_many :messages, dependent: :destroy
-  has_many :users, through: :room_users, dependent: :destroy
+  has_many :messages
+  has_many :users, through: :room_users
 
   validates :name, presence: true
 end
+# has_many :messages, dependent: :destroy
+# has_many :users, through: :room_users, dependent: :destroy
