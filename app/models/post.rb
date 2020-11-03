a6e300979f9dbe4e4f2620a0f027e29cff8f1827
11nekomamire <nekomamiremy@gmail.com>
2020-11-03 18:08:56 +0900

    バリデーション 文字数制限

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,19 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
+  
+
   with_options presence: true do
-    validates :title
-    validates :explain
+    validates :title, length: { maximum: 60 }
+    validates :explain, length: { maximum: 1000 }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :local
+    validates :local, length: { maximum: 20 }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999 }
-    validates :building
+    validates :building, length: { maximum: 20 }
     validates :image
   end
+
+  validates :other, length: { maximum: 1000 }
 
   belongs_to :user
   has_many :comments, dependent: :destroy
