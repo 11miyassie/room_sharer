1b2a22cfdcf15f7d374787d7b8aaaea5303197bb
11nekomamire <nekomamiremy@gmail.com>
2020-11-07 18:48:58 +0900

    マイページ実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname
+    validates :nickname, length: { maximum: 10 }
     validates :email
     validates :password
     validates :birthday
