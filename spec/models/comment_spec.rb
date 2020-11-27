83e4c5bca75318ea6b0a21210bce542aa7c773f6
11mrhappy <nekomamiremy@gmail.com>
2020-11-27 21:19:14 +0900

    test code comment fix

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Comment, type: :model do
       it "textが空だと登録できない" do
         @comment.text = ""
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("Comment can't be blank")
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
       end
     end
   end
